b02f4b4bf88e32c9f6c65bcc07d13c824bca2a80
deezeewhytea <arnoldjosephjr.najera@gmail.com>
2025-07-05 03:48:24 +0800

    Login screen

---
+++ landingGUI.py
@@ -0,0 +1,31 @@
+import kivy
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDButton, MDButtonIcon, MDButtonText
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.graphics import Color, Rectangle
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
+
+class LoginScreen(Screen):
+    pass
+
+class ISMISCrawler(MDApp):
+    def build(self):
+        Window.size = (614, 768)
+        Window.minimum_width = 614 
+        Window.minimum_height = 768
+        Window.maximum_width = 614
+        Window.maximum_height = 768
+        Window.resizable = False 
+        Window.set_title("ISMIS Crawler")
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.primary_hue = "700"
+        self.theme_cls.theme_style = "Dark"
+        Builder.load_file('kv/login.kv')
+        return LoginScreen()
+
+ISMISCrawler().run()
